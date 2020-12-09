eef20633eb297685c0c66f5c92c21fcdc40b3f12
Leon-Saravanja <72983290+Leon-Saravanja@users.noreply.github.com>
2020-12-09 20:25:28 +0100

    Uređivanje CSS
    uređivanje about stranice. dodovanje css

--- misli_brzo/src/views/About.vue
+++ misli_brzo/src/views/About.vue
@@ -1,23 +1,54 @@
 <template>
   <div class="about">
-    <h1><b>Bok, user</b></h1>
-        <p>Zaigraj kviz</p>
-        <p>Odaberi jedanu od punuđenih tema</p>
+    <div class="prvi_blok">
+      <h1><b>Bok, user</b></h1>
+      <p>Zaigraj kviz</p>
+      <p>Odaberi jedanu od punuđenih tema</p>
       <h6>prostor za slike</h6>
-     <h5><b>Zanimljive činjenice</b></h5>
-        <h6><b>Naslov zanimljivosti</b></h6><!--o čemu govore tjedne zanjmiljovsti, npr voda-->
-          <p>Zanimljiva činjenica #1</p>
-          <p>Zanimljiva činjenica #2</p>
-          <p>Zanimljiva činjenica #3</p>
-     <h5><b>Naslov zagonetki</b></h5>
-       <p> bla bla bla bla</p>
-       <p> #2</p>
-       <p> #3</p>
-       
+    </div>
+
+    <div class="drugi_blok">
+      <div class="zanimljive_činjenice">
+        <h5><b>Zanimljive činjenice</b></h5>
+        <div class="naslov_zanimljivosti">
+          <h6><b>Naslov zanimljivosti</b></h6>
+          <!--o čemu govore tjedne zanjmiljovsti, npr voda-->
+        </div>
+        <ol>
+          <li>zanimljivost</li>
+          <li>zanimljivost</li>
+          <li>zanimljivost</li>
+        </ol>
+
+        <div class="zagonetke">
+          <div class="naslov_zagonetki">
+            <h5><b>Naslov zagonetki</b></h5>
+            <ol>
+              <li>Zagonetka</li>
+              <li>Zagonetka</li>
+              <li>Zagonetka</li>
+              <li>Zagonetka</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
+.prvi_blok {
+  height: 1080px;
+}
 
+.drugi_blok {
+  color: white;
+  height: 1080px;
+  background-color: hotpink;
+  padding-left: 80px;
+  padding-right: 80px;
+  font-size: 20px;
+  text-align: left;
+}
 </style>
 
