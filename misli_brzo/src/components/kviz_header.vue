532b9d888174b12d50605e7a5dd2095d97b76191
Domagoj <domagojkuvezdic@gmail.com>
2020-12-24 11:38:42 +0100

    kviz_1
    Dodavanje i stiliziranje contenta na kviz_1 stranici

--- misli_brzo/src/components/kviz_header.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<h1>Component "kviz_header" is here</h1>
-</template>
-
-<script>
-export default {
-  name: "kviz_header"
-};
-</script>
--- misli_brzo/src/views/kviz_1.vue
+++ misli_brzo/src/views/kviz_1.vue
@@ -1,19 +1,48 @@
 <template>
-<div class="body">
-    <Kvizheader />
-    <h1>Hello</h1>
-</div>
+  <div class="body">
+    <h1>MisliBrzo</h1>
+    <h2>Kviz FILMOVI</h2>
+    <div class="pitanje d-flex w-50">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit?</p>
+    </div>
+    <!-- Pitanje -->
+
+    <!-- <div class="formula_1_svg">
+        <img src="@/assets/tema_formula_1.svg" alt="Formula 1 car" />
+      </div> -->
+
+    <div class="odgovor align-middle w-25">
+      <p>Odgovor A</p>
+    </div>
+    <div class="odgovor w-25">
+      <p>Odgovor B</p>
+    </div>
+    <div class="odgovor w-25">
+      <p>Odgovor C</p>
+    </div>
+    <div class="odgovor w-25">
+      <p>Odgovor D</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import kviz_header from "@/components/kviz_header.vue";
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped lang="scss">
-.body{
-    background-color: #9D5CC2;
+.body {
+  background-color: #9d5cc2;
+  height: 100vh;
+}
+.pitanje {
+  background-color: #804aa0;
+}
+.odgovor {
+  background-color: white;
+  /* width: 200px; */
+  height: 40px;
+  margin-top: 25px;
+  margin-bottom: 10px;
 }
 </style>
