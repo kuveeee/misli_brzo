c9328c1afbe6b1ff5395ead5bb09bb4b6ee31365
Domagoj <domagojkuvezdic@gmail.com>
2021-01-11 19:24:07 +0100

    Router link, logout
    Update-ani router linkovi.
    Kada je korisnik prijavljen, neće se prikazivati login i register na stranici

--- misli_brzo/src/App.vue
+++ misli_brzo/src/App.vue
@@ -3,31 +3,39 @@
     <div id="nav">
       <router-link to="/">Splash</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/signup">Signup</router-link> |
-      <router-link to="/Login">Login</router-link> |
+      <router-link v-if="!store.currentUser" to="/signup">Signup</router-link> |  
+      <router-link v-if="!store.currentUser" to="/Login">Login</router-link> |
       <router-link to="/kviz_1">Kviz 1</router-link> |
-      <a href="#" @click="logout" class="nav-link">Logout</a>
+      <a v-if="store.currentUser" href="#" @click="logout" class="nav-link">Logout</a>
     </div>
     <router-view />
   </div>
 </template>
 
 <script>
+import store from '@/store';
 import { firebase } from "@/firebase";
 
 firebase.auth().onAuthStateChanged((user) => {
   //Metoda koja prati jel user prijavljen ili odjavljen
   if (user) {
     // User is signed in.
-    console.log("***", user.mail);
+    console.log("***", user.email);
+    store.currentUser = user.email;
   } else {
     // No user is signed in.
     console.log("*** No user");
+    store.currentUser = '';
   }
 });
 
 export default {
   name: "App",
+  data() {
+    return {
+      store,
+    };
+  },
 
   methods: {
     logout() {
